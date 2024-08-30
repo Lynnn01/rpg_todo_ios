7e6f89de413d1ed011ef5b924127f38b8da720be
Lynnn01 <jetsadathegame@gmail.com>
2024-08-30 00:20:27 -0700

    Initial Commit

---
+++ rpg_todo_ios/rpg_todo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpg_todo_iosApp.swift
+//  rpg_todo_ios
+//
+//  Created by admin on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct rpg_todo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
